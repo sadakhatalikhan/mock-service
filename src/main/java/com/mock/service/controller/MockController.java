b99ea89754ba370e4af4e5017fdb14b2ac5b87eb
sadakhatalikhan <sadakhat11.khan@gmail.com>
2025-04-08 08:09:32 +0530

    Initial commit with mock controller changes

---
+++ src/main/java/com/mock/service/controller/MockController.java
@@ -0,0 +1,21 @@
+package com.mock.service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/response")
+public class MockController {
+
+    @GetMapping("/200")
+    public ResponseEntity<String> response200String() {
+        return ResponseEntity.ok().body("200");
+    }
+
+    @GetMapping("/500")
+    public ResponseEntity<String> response500String() {
+        return ResponseEntity.internalServerError().build();
+    }
+}
